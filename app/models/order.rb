9045bf10b4c6fbb9baac893d739077b7604caa20
lizzerdrix <liz@lizbaillie.com>
2014-10-10 10:47:27 -0400

    populates order model with help from looking at selfstarter

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,38 @@
 class Order < ActiveRecord::Base
+  before_validation :generate_uuid!, :on => :create
+  belongs_to :user
+  self.primary_key = 'uuid'
+
+  def self.goal
+    1000
+  end
+
+  def self.percent
+    (Order.revenue.to_f / Order.goal.to_f) * 100.to_f
+  end
+
+  def self.sponsors
+    Order.completed.count
+  end
+
+  def self.revenue
+    Order.completed.sum(:price).to_f 
+  end
+
+  def self.next_order_number
+    if Order.count > 0
+      Order.order("number DESC").limit(1).first.number.to_i + 1
+    else
+      1
+    end
+  end
+
+  def generate_uuid!
+    begin
+      self.uuid = SecureRandom.hex(16)
+    end while Order.find_by(:uuid => self.uuid).present?
+  end
+
+  validates_presence_of :name, :price, :user_id
+
 end
