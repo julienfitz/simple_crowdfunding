60269c2e52adf238a509d648a455457b840f2480
lizzerdrix <liz@lizbaillie.com>
2014-10-16 13:22:05 -0400

    alters orders controller a bit, trimming the fat

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,35 +1,32 @@
 class OrdersController < ApplicationController
 
-  def index
-    @orders = Order.all
-    #@order = Order.find_or_create_by(params[:email])
+  def show
   end
 
   def checkout
-    @sponsors = Order.all.count
-  end
+    @order = Order.create(params[:level_num])
 
-  def create
-    @order = Order.find_or_create_by(params[:email])
-
+    respond_to do |format|
+      format.html { redirect_to order_path(@order), notice: 'Thanks for your order! Please email me at liz@lizbaillie.com if you have any questions or concerns.' }
+    end
       # Amount in cents
-      @amount = @order.price
+      #@amount = @order.price
 
-      customer = Stripe::Customer.create(
-        :email => 'example@stripe.com',
-        :card  => params[:stripeToken]
-      )
+    #   customer = Stripe::Customer.create(
+    #     :email => 'example@stripe.com',
+    #     :card  => params[:stripeToken]
+    #   )
 
-      charge = Stripe::Charge.create(
-        :customer    => customer.id,
-        :amount      => @amount,
-        :description => 'Rails Stripe customer',
-        :currency    => 'usd'
-      )
+    #   charge = Stripe::Charge.create(
+    #     :customer    => customer.id,
+    #     :amount      => @amount,
+    #     :description => 'Rails Stripe customer',
+    #     :currency    => 'usd'
+    #   )
 
-    rescue Stripe::CardError => e
-      flash[:error] = e.message
-      redirect_to root_path
+    # rescue Stripe::CardError => e
+    #   flash[:error] = e.message
+    #   redirect_to root_path
 
   end
 
@@ -40,7 +37,7 @@ class OrdersController < ApplicationController
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def order_params
-      params.require(:order).permit(:token, :transaction_id, :address_one, :address_two, :city, :state, :zip, :country, :status, :number, :uuid, :user_id, :price, :phone, :name)
-    end
+    # def order_params
+    #   params.require(:order).permit(:token, :transaction_id, :address_one, :address_two, :city, :state, :zip, :country, :status, :number, :uuid, :user_id, :price, :phone, :name)
+    # end
 end
