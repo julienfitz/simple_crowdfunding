d22de48e83fba2087581f4cb2a6609fec8d58e08
lizzerdrix <liz@lizbaillie.com>
2014-10-16 15:00:38 -0400

    minor superficial changes to seed Items

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ sponsorships = Item.create([{
   {
     name: 'Level Two: Flat Stanley',
     price: 50,
-    description: 'I will mail you a hand-drawn thank you note and draw a picture of your face. While at RubyConf, I will take at least one selfie with the drawing of your face, <a href="http://en.wikipedia.org/wiki/Flat_Stanley#The_Flat_Stanley_Project">Flat Stanley style</a>.'
+    description: 'I will mail you a hand-drawn thank you note and draw a picture of your face. While at RubyConf, I will take at least one selfie with the drawing of your face, Flat Stanley style.'
   },
   {
     name: 'Level Three: Flat Stanley PLUS!',
@@ -24,6 +24,6 @@ sponsorships = Item.create([{
   {
     name: 'Level Four: MASTER OF THE UNIVERSE!',
     price: 300,
-    description: "Get every single wonderful thing from the previous levels, PLUS I will make a very special 'thank you' video calling you out by name!"
+    description: "Get every single wonderful thing from the previous levels, PLUS I will make a very special musical 'thank you' video calling you (and any other sponsors at this level) out by name! I'll also draw you up a custom certificate, officially denoting your status as a MASTER OF THE UNIVERSE!"
   }
   ])
