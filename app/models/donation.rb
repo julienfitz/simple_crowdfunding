42086e15d54bf37578253492383d87495a80f869
lizzerdrix <liz@lizbaillie.com>
2014-10-19 22:56:18 -0400

    adds how many days are left to index page

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -1,8 +1,10 @@
 class Donation < ActiveRecord::Base
   has_many :charges
 
-
-  DONATION_LEVELS = ["Level One - $20","Level Two - $50",
-                     "Level Three - $100", "Level Four - $300"]
+  def self.days_left
+    start_date = DateTime.now
+    end_date = DateTime.parse "2014-11-07 00:00:00 +0500"
+    (end_date - start_date).to_i
+  end
 
 end
